4d647607221f1fd6155b58795aa95aef906c8164
heleenpaulus <heleenpaulus@localhost.localdomain>
2022-10-08 14:34:04 +0200

    name added second time

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,6 +2,4 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 <<<<<<< HEAD
 print "A change by Heleen is coming very soon!\n";
-=======
-print "A change is a coming by the modest Eric Michiels!\n";
->>>>>>> 9b9de23d452ce17bb37baea9178a22412213ab6c
+
